e9663e742fec88952803461a69e2f97d61867d9e
HrishikeshWadile <hrishikeshwadile2510@gmail.com>
2025-04-14 00:19:11 +0530

    modified firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
